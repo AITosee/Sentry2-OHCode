cbc1080b70db92098ca43dbff9f225c3b3b4e37a
feng <succeed_happu@outlook.com>
2025-03-14 07:24:49 +0800

    docs(sentry2-ohcode): 更新项目文档和配置
    - 移除 UART 模式选项
    - 添加 Sentry2 视觉传感器的描述和帮助 URL
    - 更新项目版本号至 2.1.2

--- src/blocks/block.js
+++ src/blocks/block.js
@@ -246,7 +246,7 @@ export const vision_obj_card_dict = function () {
 // Sentry2
 export const Sentry2Begin = {
     init: function () {
-        var mode_objs = [["I2C", "i2c0"],["UART", "uart2"]];
+        var mode_objs = [["I2C", "i2c0"]];
 
         this.appendDummyInput()
             .appendField(
